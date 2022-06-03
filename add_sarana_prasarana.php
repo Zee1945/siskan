4baa5fdb305208c210344b7dc65424f29b0fac56
Zul Fachrie <47557182+Zee1945@users.noreply.github.com>
2022-06-03 17:25:21 +0700

    update II

--- add_sarana_prasarana.php
+++ add_sarana_prasarana.php
@@ -7,16 +7,12 @@ $tahun = $_POST['tahun'];
 $jenis_sarana = $_POST['jenis-sarana'];
 $keterangan = $_POST['keterangan'];
 $nama_gambar = $_FILES['gambar-sarana']['name'];
-$ekstensi_diperbolehkan = array('png', 'jpg', 'jpeg');
-$x = explode('.', $nama_gambar);
-$ekstensi = strtolower(end($x));
+
 $ukuran    = $_FILES['gambar-sarana']['size'];
 $file_tmp = $_FILES['gambar-sarana']['tmp_name'];
 
 // echo $nama_gambar;
 
-
-if (in_array($ekstensi, $ekstensi_diperbolehkan) === true) {
     if ($ukuran < 20440700) {
         move_uploaded_file($file_tmp, 'assets/img/sarana/' . $nama_gambar);
         $query = mysqli_query($mysqli, "INSERT INTO sarana_prasarana(
@@ -38,9 +34,7 @@ if (in_array($ekstensi, $ekstensi_diperbolehkan) === true) {
     } else {
         echo 'UKURAN FILE TERLALU BESAR';
     }
-} else {
-    echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
-}
+
 
 // Fetch all users data from database
 header("location:manajemen.php");
